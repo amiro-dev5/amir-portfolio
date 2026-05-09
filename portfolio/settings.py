bddf3f498308d439209029afcb390707814de58d
Amir Seid <sj3398580@gmail.com>
2026-05-10 02:53:52 +0300

    Add Pillow and fix default auto field

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -140,3 +140,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
