1e9860c74ad42e72f7e5137d536a90eca3f98ee4
Amir Seid <sj3398580@gmail.com>
2026-05-10 22:40:01 +0300

    changing to postgrsql

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -14,3 +14,12 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 
 application = get_wsgi_application()
+
+import os
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+if not User.objects.filter(username='amir_admin').exists():
+    User.objects.create_superuser('amir_admin', 'amir@example.com', 'Amir@2026_Postgres')
+    print("Superuser 'amir_admin' created successfully!")
