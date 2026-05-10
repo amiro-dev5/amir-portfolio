38df69dd238832e729ba9ec380c2ce1f8ea94cb6
Amir Seid <sj3398580@gmail.com>
2026-05-10 16:09:44 +0300

    Hardcode settings and update email port

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -136,8 +136,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587           
-EMAIL_USE_TLS = True           
+EMAIL_PORT = 2525
+EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST_USER ='crpor1487@gmail.com'
 EMAIL_HOST_PASSWORD = 'ohfhzhdhnnhqolqt'
