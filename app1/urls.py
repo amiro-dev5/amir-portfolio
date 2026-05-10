0d82da0ed326250e6c36fd4b58183461463fa1f2
Amir Seid <sj3398580@gmail.com>
2026-05-10 18:35:24 +0300

    Emergency admin creation

--- app1/urls.py
+++ app1/urls.py
@@ -4,3 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
 ]
+
+from django.contrib.auth.models import User
+
+def create_admin_once():
+    if not User.objects.filter(username='amir').exists():
+        User.objects.create_superuser('amir', 'aad392001@gmail.com', 'Amir@2026')
+        print("Admin created successfully!")
+
+create_admin_once()
